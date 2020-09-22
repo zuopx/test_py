dae18da86bedcf851784a80ab0d5e592c920bf79
Percy Cho <zuopxsm@outlook.com>
2020-09-23 00:28:01 +0800

    None

--- src/control_flow/coroutine/delegating_generator.py
+++ src/control_flow/coroutine/delegating_generator.py
@@ -28,17 +28,43 @@ def grouper(results, key):  # delegating generator
     while True:
         results[key] = yield from averager()
 
+
 def test_averager():
     avg = averager()
     next(avg)
     avg.send(1)
     avg.send(2)
     avg.send(3)
-    result = avg.send(None)
-    # assert result == Result(3, 2)
+    with pytest.raises(StopIteration):
+        avg.send(None)
+
+
+data = {
+    'girls;kg':
+    [40.9, 38.5, 44.3, 42.2, 45.2, 41.7, 44.5, 38.0, 40.6, 44.5],
+    'girls;m':
+    [1.6, 1.51, 1.4, 1.3, 1.41, 1.39, 1.33, 1.46, 1.45, 1.43],
+    'boys;kg':
+    [39.0, 40.8, 43.2, 40.8, 43.1, 38.6, 41.4, 40.6, 36.3],
+    'boys;m':
+    [1.38, 1.5, 1.32, 1.25, 1.37, 1.48, 1.25, 1.49, 1.46],
+}
+
+
+def test_delegating_generator():
+    results = {}
+    for key, values in data.items():
+        group = grouper(results, key)  # 返回一个generator object
+        next(group)
+        for value in values:
+            group.send(value)
+        group.send(None)
+
+    print(results)
+
 
 if __name__ == "__main__":
     prefix = __file__ + '::'
-    func = 'test_averager'
+    func = 'test_delegating_generator'
+    # func = 'test_averager'
     pytest.main([prefix + func, '-s'])
-
